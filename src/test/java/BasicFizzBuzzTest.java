74c3523678ccab7bf4f4482c864c715a8d6db076
vijayjavaprgmr <vijayjavaprgmr@gmail.com>
2021-01-15 13:41:13 +0530

    Code refactor: removed object creation redundancy

--- src/test/java/BasicFizzBuzzTest.java
+++ src/test/java/BasicFizzBuzzTest.java
@@ -1,20 +1,26 @@
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 
 import com.bnpp.kata.BasicFizzBuzzGame;
 
 public class BasicFizzBuzzTest {
+	BasicFizzBuzzGame basicFizzBuzz = null;
+
+	@Before
+	public void initializeBasicFizzBuzzGameObj() {
+		basicFizzBuzz = new BasicFizzBuzzGame();
+	}
+
 	@Test
 	public void testNormalNumbersReturnSameNumber() {
-		BasicFizzBuzzGame basicFizzBuzz = new BasicFizzBuzzGame();
 		assertThat(basicFizzBuzz.getFizzBuzzScore(1), is("1"));
 	}
 
 	@Test
 	public void testMultiplesofThreeReturnFizz() {
-		BasicFizzBuzzGame basicFizzBuzz = new BasicFizzBuzzGame();
 		assertThat(basicFizzBuzz.getFizzBuzzScore(123), is("fizz"));
 	}
 }
